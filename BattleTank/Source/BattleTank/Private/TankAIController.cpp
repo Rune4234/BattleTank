ccad0e12b172e749d6b9ee6c0dd86377c7372caf
Ian <Rune4234@yahoo.com>
2018-04-01 15:20:43 -0700

    BT17 AIController Finds PlayerController

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,17 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "TankAIController.h"
+#include "TankAIController.h" // Must be first include.
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	ATank* ControlledTank = GetControlledTank();
-	if (!ControlledTank) {
-		UE_LOG(LogTemp, Error, TEXT("AIController not posessing a tank."))
+	ATank* PlayerTank = GetPlayerTank();
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Error, TEXT("AIController not finding player."))
 	}
 	else {
-		UE_LOG(LogTemp, Warning, TEXT("AIController posessing: %s"), *(ControlledTank->GetName()))
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *(PlayerTank->GetName()))
 	}
 }
 
@@ -19,4 +21,15 @@ void ATankAIController::BeginPlay()
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) {
+		return nullptr;
+	} else {
+		return Cast<ATank>(PlayerPawn);
+	}
 }
