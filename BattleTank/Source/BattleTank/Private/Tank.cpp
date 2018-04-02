556ed1c4c74f4350f346f39a014f0c9fd1c6ed0c
Ian <Rune4234@yahoo.com>
2018-04-02 11:39:19 -0700

    BT23 Created TankAimingComponent

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -1,14 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tank.h"
+#include "GameFramework/Actor.h"
+#include "TankAimingComponent.h"
 
 
 // Sets default values
 ATank::ATank()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// No need to protect pointers as added at construction
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 }
 
 
@@ -38,6 +41,5 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-	FString OurTankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s is aiming at: %s"), *(OurTankName), *(HitLocation.ToString()))
+	TankAimingComponent->AimAt(HitLocation);
 }
---
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TankAimingComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API UTankAimingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void AimAt(FVector WorldSpaceAim);
+
+private:	
+	UTankAimingComponent();
+};
