45df3a8682ba43151eb5d944b898a40018144327
Ian <Rune4234@yahoo.com>
2018-04-07 10:41:22 -0700

    BT41 Added physics material to tank tracks.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -13,7 +13,7 @@ void UTankTrack::SetThrottle(float Throttle)
 	//TODO we need to clamp this shit so player cant go faster than we allow.
 	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
-	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
