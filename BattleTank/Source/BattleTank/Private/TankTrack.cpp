f83f7abb5de3591242b28f93573eb48b6bc5cef9
Ian <Rune4234@yahoo.com>
2018-04-07 09:49:31 -0700

    BT40 AddForceAtLocation

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -11,6 +11,10 @@ void UTankTrack::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("%f: %s throttle: %f"), Time, *Name, Throttle)
 
 	//TODO we need to clamp this shit so player cant go faster than we allow.
+	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
 
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -13,9 +13,13 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 	GENERATED_BODY()
 	
 public:
-	// Sets a throttle between -1 and +1.
 	UFUNCTION(BlueprintCallable, Category = Input)
+		// Sets a throttle between -1 and +1.
 	void SetThrottle(float Throttle);
 	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	// Maximum force per track in Newtons.
+	float TrackMaxDrivingForce = 400000.0f; //TODO figure out sensible value, this one assumes 40 tonne tank and 1g accelleration.
 	
 };
